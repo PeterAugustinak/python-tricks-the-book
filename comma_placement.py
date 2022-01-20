8b5c29221a739aedfcc537a958c320b0fdd90a0a
p-a <peter.augustinak@gmail.com>
2022-01-20 16:41:03 +0100

    Complacement with comma placement - using new line and comma after last item in arrays

---
+++ comma_placement.py
@@ -0,0 +1,27 @@
+# standard using of list, dict, set
+l = ['a', 'b', 'c']
+d = {'a': 1, 'b': 2, 'c': 3}
+s = {1, 2, 3}
+
+# for better git diff readability, it is better to write every item in new
+# line plus letting the comma always also after the last item. Then when
+# editing (adding new item into the array), only one line is added and no
+# line is edited
+
+lc = [
+    'a',
+    'b',
+    'c',
+]
+
+dc = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+}
+
+sc = {
+    1,
+    2,
+    3,
+}
