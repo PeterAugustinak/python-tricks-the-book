6361003b67c29f467131afd3f638192ee40a629e
p-a <peter.augustinak@gmail.com>
2022-01-24 16:48:58 +0100

    Fun with *args and **kwargs - args and kwargsexample in function, class and decorator level

---
+++ src/args_and_kwargs.py
@@ -0,0 +1,51 @@
+"""Fun with *args and ** kwargs"""
+import functools
+
+"""Function level"""
+def foo(required, *args, **kwargs):
+    print(required)
+    if args:
+        print(args)
+    if kwargs:
+        print(kwargs)
+
+# print(foo())
+foo("Hello")
+foo("Hello", 1, 2, 3)
+foo("Hello", 1, 2, 3, key1="value", key2=999)
+
+
+"""Class level"""
+class Car:
+
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+class AlwaysBlueCar(Car):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.color = 'blue'
+
+car = AlwaysBlueCar('green', 49343)
+print(car.color)
+
+
+"""Decorators level"""
+def trace(func):
+    @functools.wraps(func)
+    def decorated_function(*args, **kwargs):
+        print(func, args, kwargs)
+        result = func(*args, **kwargs)
+        print(result)
+    return decorated_function
+
+@trace
+def greet(greeting, name):
+    return f'{greeting} {name}'
+
+greet('Hello', 'Bob')
+
+
+
