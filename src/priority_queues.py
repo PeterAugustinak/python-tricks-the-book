d5440aa1447ac07bbd315e28363ec51b6694cae2
PeterAugustinak <peter.augustinak@gmail.com>
2022-03-15 16:43:19 +0100

    5.7 Priority Queues

---
+++ src/priority_queues.py
@@ -0,0 +1,43 @@
+"""5.7 Priority Queues"""
+
+"""list - Maintaining a Manually Sorted Queue"""
+
+q = []
+q.append((2, 'code'))
+q.append((1, 'eat'))
+q.append((3, 'sleep'))
+
+# NOTE: Remember to re-sort every time a new element is inserted
+q.sort()
+
+while q:
+    next_item = q.pop()
+    print(next_item)
+
+"""heapq - List-Based Binary Heaps"""
+print()
+
+import heapq
+
+h = []
+heapq.heappush(h, (2, 'code'))
+heapq.heappush(h, (1, 'eat'))
+heapq.heappush(h, (3, 'sleep'))
+
+while h:
+    next_item = heapq.heappop(h)
+    print(next_item)
+
+"""queue.PriorityQueue - Beautiful Priority Queues"""
+print()
+
+from queue import PriorityQueue
+
+pq = PriorityQueue()
+pq.put((2, 'code'))
+pq.put((1, 'eat'))
+pq.put((3, 'sleep'))
+
+while not pq.empty():
+    next_item = pq.get()
+    print(next_item)
