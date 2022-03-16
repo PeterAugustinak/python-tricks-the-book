9d116cbaacd8ff2e82c8b8c72e85a7e9865b22bb
PeterAugustinak <peter.augustinak@gmail.com>
2022-03-16 16:52:59 +0100

    8.1 Exploring Python Modules and Objects

---
+++ src/modules_objects_exploring.py
@@ -0,0 +1,27 @@
+"""8.1 Exploring Python MOdules and Objects"""
+
+
+import datetime
+inspect_datetime = dir(datetime)
+print(inspect_datetime)
+
+inspect_datetime_date = dir(datetime.date)
+print(inspect_datetime_date)
+
+# inspect only specific attrs
+only_date_related = [_ for _ in dir(datetime) if 'date' in _.lower()]
+print(only_date_related)
+
+# inspect only public
+only_public = [_ for _ in dir(datetime) if not (_.startswith('_')
+                                                or _.startswith('__'))]
+print(only_public)
+
+# check help
+# helper = help(datetime)
+# print(helper)
+
+specific_helper = help(datetime.date.fromtimestamp)
+print(specific_helper)
+
+# print(help(dir))
